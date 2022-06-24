2fc0dff0c421529004b868b4f63e616bb1b659f4
CesarOliiveira <cesaroliiveira8@gmail.com>
2022-06-24 12:01:55 -0300

    Atividade PW Migrations

---
+++ database/migrations/2022_05_25_212653_create_order_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('quantity');
+            $table->float('sub-total');
+
+            $table->integer('id_user');
+            $table->foreign('id_user')->references('id')->on('user');
+            
+            $table->integer('id_product');
+            $table->foreign('id_product')->references('id')->on('product');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+};
